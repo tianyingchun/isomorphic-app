3d6b93ba240bbf683453bbbff8ea61391ccd97a7
glodon <glodon@glodondeiMac.local>
2015-07-27 18:33:12 +0800

    updates...

--- app/BootStrap.jsx
+++ app/BootStrap.jsx
@@ -10,19 +10,19 @@ var BootStrap = React.createClass({
   render: function () {
     return (
       <html>
-        <head lang="en">
-          <base href="/" />
-          <meta charSet="utf-8" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <title>The isomorphic app sample</title>
-          <link href="static/built/styles/bootstrap.css" rel="stylesheet" />
-          <script type="text/javascript" src="/static/built/vendor/react.js"></script>
-          <script type="text/javascript" src="/static/built/app/bundle.js"></script>
-        </head>
-        <body>
-          <App />
-        </body>
+      <head lang="en">
+        <base href="/" />
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>The isomorphic app sample</title>
+        <link href="static/built/styles/bootstrap.css" rel="stylesheet" />
+        <script type="text/javascript" src="/static/built/vendor/react.js"></script>
+        <script type="text/javascript" src="/static/built/app/bundle.js"></script>
+      </head>
+      <body>
+        <App />
+      </body>
       </html>
     );
   }
